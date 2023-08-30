436c44a3161e6de4c4fc04ce4c50df0a835156d1
david-dom <113909984+david-dom@users.noreply.github.com>
2023-08-29 23:42:36 -0500

    My commit message

---
+++ src/agua/pages/screen1.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './screen1.module.css';
+import SimpleButton from '../components/customCode/simpleButton.jsx';
+
+
+
+function Screen1() {
+    
+
+    return(
+        <div className={styles["Screen1"]}>
+            
+<SimpleButton   />
+        </div>
+    );
+}
+
+export default Screen1;
+            
